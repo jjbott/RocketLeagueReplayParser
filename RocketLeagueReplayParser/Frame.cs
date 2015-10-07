a03e25ef83d04c896f6bc9ab45fb62a0e7a570ab
jjbott <jjbott@gmail.com>
2015-10-06 21:39:46 -0500

    Capture the unknown bits in a frame more reliably

--- RocketLeagueReplayParser/Frame.cs
+++ RocketLeagueReplayParser/Frame.cs
@@ -41,50 +41,12 @@ namespace RocketLeagueReplayParser
             while ( (lastActorState == null || lastActorState.Complete) && br.ReadBit() )// TODO: Switch to while(br.ReadBit()) // bit=1 means replicating another actor
             {
                 lastActorState = ActorState.Deserialize(br);
-
-                if ( !lastActorState.Complete)
-                {
-                    while(!br.EndOfStream)
-                    {
-                        f.UnknownBits.Add(br.ReadBit());
-                    }
-                }
-
                 f.ActorStates.Add(lastActorState);
-                /*
-                var actorId = br.ReadInt32FromBits(10);
-                var channelStateOpen = br.ReadBit();
+            }
 
-                if (channelStateOpen)
-                    continue;
-
-                var isNewActor = br.ReadBit();
-                if (!isNewActor)
-                {
-                    while (br.ReadBit()) //Has some properties to read!!!
-                    {
-                        //read 6 bits = property id
-                        //the rest is a data which is variable depending on size of property id
-                    }
-                    //check for and read the properties
-                    continue;
-                }
-
-                //parse new actor!!!!!!!
-                //read a variable sized int to get the type //typeIndex = readVarInt();
-                //var ints are read 8 bits at a time, the first 7 bits are data (be careful about the order of these), the 8th bit signifies to keep reading another 8 bits (if 1) or to stop (if 0).
-                //from here we can get the string representing this type by looking it up in the objects list given the type index
-
-                bool keepReading;
-
-                do
-                {
-                    byte[] varInt = br.ReadBitsAsBytes(8);
-                    keepReading = (varInt[0] & 0x01) == 0x01; //Check 8th bit to see if we should expect another thingy
-
-                    var typeIndex = 0xfe & varInt[0]; //Get first 7 bits which represents the type index
-                } while (keepReading);
-                 * */
+            while (!br.EndOfStream)
+            {
+                f.UnknownBits.Add(br.ReadBit());
             }
 
             return f;
