75316c8a9e9f413b3aeb4273b285c7dd841ad8b5
jjbott <jjbott@gmail.com>
2016-01-20 19:58:16 -0600

    Fixing up RBState unknowns

--- RocketLeagueReplayParser/NetworkStream/RigidBodyState.cs
+++ RocketLeagueReplayParser/NetworkStream/RigidBodyState.cs
@@ -8,23 +8,23 @@ namespace RocketLeagueReplayParser.NetworkStream
 {
     public class RigidBodyState
     {
-        public bool UnknownDataMissing { get; private set; } // Rename once I figure out the unknown data
+        public bool Sleeping { get; private set; }
         public Vector3D Position { get; private set; }
-        public Vector3D Rotation { get; private set; }
-        public Vector3D UnknownVector1 { get; private set; }
-        public Vector3D UnknownVector2 { get; private set; }
+        public Vector3D Rotation { get; private set; } // Pitch/Yaw/Roll
+        public Vector3D LinearVelocity { get; private set; }
+        public Vector3D AngularVelocity { get; private set; }
 
         public static RigidBodyState Deserialize(BitReader br)
         {
             var rbs = new RigidBodyState();
-            rbs.UnknownDataMissing = br.ReadBit();
+            rbs.Sleeping = br.ReadBit();
             rbs.Position = Vector3D.Deserialize(br);
             rbs.Rotation = Vector3D.DeserializeFixed(br);
 
-            if (!rbs.UnknownDataMissing)
+            if (!rbs.Sleeping)
             {
-                rbs.UnknownVector1 = Vector3D.Deserialize(br);
-                rbs.UnknownVector2 = Vector3D.Deserialize(br);
+                rbs.LinearVelocity = Vector3D.Deserialize(br);
+                rbs.AngularVelocity = Vector3D.Deserialize(br);
             }
 
             return rbs;
@@ -32,9 +32,9 @@ namespace RocketLeagueReplayParser.NetworkStream
 
         public override string ToString()
         {
-            if ( !UnknownDataMissing )
+            if ( !Sleeping )
             {
-                return String.Format("Position: {0} Rotation {1} UnknownVector1 {2} UnknownVector1 {3}", Position, Rotation, UnknownVector1, UnknownVector2);
+                return String.Format("Position: {0} Rotation {1} LinearVelocity {2} AngularVelocity {3}", Position, Rotation, LinearVelocity, AngularVelocity);
             }
             return String.Format("Position: {0} Rotation {1}", Position, Rotation);
         }
