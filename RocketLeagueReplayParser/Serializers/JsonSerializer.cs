2f7296466babdbeaad3fb69b2574329ae70319ae
jjbott <jjbott@gmail.com>
2016-01-24 23:16:22 -0600

    Pack actor state properties better when serializing

--- RocketLeagueReplayParser/Serializers/JsonSerializer.cs
+++ RocketLeagueReplayParser/Serializers/JsonSerializer.cs
@@ -30,46 +30,6 @@ namespace RocketLeagueReplayParser.Serializers
             public List<object> Data { get; set; }
         }
 
-        public class ActorStatePropertyConverter : JavaScriptConverter
-        {
-            public override IEnumerable<Type> SupportedTypes
-            {
-                get
-                {
-                    return new[] { typeof(ActorStateProperty) };
-                }
-            }
-
-            public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
-            {
-                throw new NotSupportedException();
-            }
-
-            public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
-            {
-                ActorStateProperty p = (ActorStateProperty)obj;
-                IDictionary<string, object> serialized = new Dictionary<string, object>();
-
-                if ( p.Data.Count == 1 )
-                {
-                    serialized[p.Name] = p.Data[0];
-                }
-                else
-                {
-                    serialized[p.Name] = p.Data;
-                }
-
-                // Adding extra info in this case to convert to a flag. Is that weird? I dunno...
-                // Conflicted between creating raw JSON, and creating "nice" JSON
-                if (p.Name == "TAGame.CarComponent_TA:ReplicatedActive")
-                {
-                    serialized["TAGame.CarComponent_TA:Active"] = (Convert.ToInt32(p.Data[0]) % 2) != 0;
-                }
-
-                return serialized;
-            }
-        }
-
         public string Serialize(Replay replay)
         {
             var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
