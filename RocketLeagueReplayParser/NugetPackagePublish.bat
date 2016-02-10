98f76a9f8d3cea938e2cdf8953326c74655940cc
jjbott <jjbott@gmail.com>
2016-02-09 21:09:14 -0600

    Adding some quick helpers for publishing to nuget

---
+++ RocketLeagueReplayParser/NugetPackagePublish.bat
@@ -0,0 +1,6 @@
+@echo off
+call ..\..\SetNugetApiKey.bat
+del *.nupkg
+nuget pack RocketLeagueReplayParser.csproj -Build -Prop Configuration=Release
+nuget setApiKey %NUGET_API_KEY%
+nuget push *.nupkg
