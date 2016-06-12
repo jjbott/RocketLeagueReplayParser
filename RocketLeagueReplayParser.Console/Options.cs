8a030f27fcbeab1ce43f84506ff9091786e7721d
jjbott <jjbott@gmail.com>
2016-06-12 12:36:51 -0500

    Adding a simple console front end

---
+++ RocketLeagueReplayParser.Console/Options.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketLeagueReplayParser.Console
+{
+    public class Options
+    {
+        [Option('r', "raw", Default = false, HelpText = "Output 'raw' JSON")]
+        public bool Raw { get; set; }
+
+        // Omitting long name, default --verbose
+        [Option("fileoutput", HelpText = "Enable file output to current directory. By default writes to stdout")]
+        public bool FileOutput { get; set; }
+
+        [Option('d', "directory", Default = false, HelpText = "Directory mode: Parses all replay files in the input directory.")]
+        public bool DirectoryMode { get; set; }
+
+        [Value(0, Required = true, MetaName = "input", HelpText = "The replay file/directory to parse")]
+        public string Input { get; set; }
+    }
+}
---
+++ RocketLeagueReplayParser.Console/Program.cs
@@ -0,0 +1,96 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketLeagueReplayParser.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Any())
+            {
+                var result = Parser.Default.ParseArguments<Options>(args);
+                result.MapResult(
+                    options => Process(options),
+                    _ => 1);
+            }
+            else
+            {
+                Parser.Default.ParseArguments<Options>(new string[] { "--help" });
+            }
+        }
+
+        private static int Process(Options o)
+        {
+            var serializer = new Serializers.JsonSerializer();
+            var inputFiles = new List<string>();
+
+            if (o.DirectoryMode)
+            {
+                if ( !Directory.Exists(o.Input) )
+                {
+                    System.Console.Error.WriteLine("Directory does not exist");
+                    return 1;
+                }
+
+                inputFiles.AddRange(Directory.EnumerateFiles(o.Input, "*.replay"));
+
+                if (!inputFiles.Any())
+                {
+                    System.Console.Error.WriteLine("No replay files found in the specified directory");
+                    return 1;
+                }
+
+                inputFiles = inputFiles.Select(f => Path.Combine(o.Input, f)).ToList();
+            }
+            else
+            {
+                inputFiles.Add(o.Input);
+            }
+
+            foreach (var file in inputFiles)
+            {
+                if (!File.Exists(file))
+                {
+                    System.Console.Error.WriteLine(string.Format("Specified replay file {0} does not exist", file));
+                    return 1;
+                }
+
+                if (o.FileOutput)
+                {
+                    System.Console.Write("Processing replay " + file + "...");
+                }
+
+                var replay = Replay.Deserialize(file);
+
+                string json;
+                if (o.Raw)
+                {
+                    json = serializer.SerializeRaw(replay);
+                }
+                else
+                {
+                    json = serializer.Serialize(replay);
+                }
+
+                if (o.FileOutput)
+                {
+                    var filename = Path.GetFileNameWithoutExtension(file) + ".json";
+                    File.WriteAllText(filename, json);
+                    System.Console.WriteLine("Complete!");
+                }
+                else
+                {
+                    System.Console.Write(json);
+                }
+            }
+
+            return 0;
+        }
+    }
+}
