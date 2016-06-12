949d8d135a372024d6f0bdf2fb19ff703e102fd7
jjbott <jjbott@gmail.com>
2016-06-12 13:03:35 -0500

    Update nuget publish script for updated nuget.exe path

--- RocketLeagueReplayParser/NugetPackagePublish.bat
+++ RocketLeagueReplayParser/NugetPackagePublish.bat
@@ -1,7 +1,7 @@
 @echo off
 call ..\..\SetNugetApiKey.bat
 del *.nupkg
-nuget pack RocketLeagueReplayParser.csproj -Build -Prop Configuration=Release
-nuget setApiKey %NUGET_API_KEY%
-nuget push *.nupkg
+..\..\nuget pack RocketLeagueReplayParser.csproj -Build -Prop Configuration=Release
+..\..\nuget setApiKey %NUGET_API_KEY%
+..\..\nuget push *.nupkg
 pause
