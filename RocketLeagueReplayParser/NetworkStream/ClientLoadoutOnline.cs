c61e3720c00c67081dd2b795024298a72a12598f
jjbott <jjbott@gmail.com>
2016-06-25 22:23:29 -0500

    ClientLoadoutOnline data is stored and can be re-serialized. Still dont know what the data is telling me exactly

--- RocketLeagueReplayParser/NetworkStream/ClientLoadoutOnline.cs
+++ RocketLeagueReplayParser/NetworkStream/ClientLoadoutOnline.cs
@@ -8,41 +8,47 @@ namespace RocketLeagueReplayParser.NetworkStream
 {
     public class ClientLoadoutOnline
     {
-        public byte Version { get; private set; }
-        public UInt32 Unknown1 { get; private set; }
-        public UInt32 Unknown2 { get; private set; }
-        public byte Unknown3 { get; private set; }
-        public byte Count { get; private set; }
+        public List<List<ClientLoadoutOnlineThing>> ThingLists { get; private set; }
+
         public static ClientLoadoutOnline Deserialize(BitReader br)
         {
-            var p = br.Position;
-            bool dump = false;
             var clo = new ClientLoadoutOnline();
-
-            // TODO: Store all of this stuff. It's just throwing everything away right now.
-            var count1 = br.ReadByte();
-            for (int i = 0; i < count1; ++i)
+            clo.ThingLists = new List<List<ClientLoadoutOnlineThing>>();
+            
+            var listCount = br.ReadByte();
+            for (int i = 0; i < listCount; ++i)
             {
-                var count2 = br.ReadByte();
-                for (int j = 0; j < count2; ++j)
+                var thingList = new List<ClientLoadoutOnlineThing>();
+
+                var thingCount = br.ReadByte();
+                for (int j = 0; j < thingCount; ++j)
                 {
-                    br.ReadInt32();
-                    br.ReadUInt32Max(27); // made up number, need more data
+                    thingList.Add(ClientLoadoutOnlineThing.Deserialize(br));
+
                     if ( i >= 21 )
                     {
-                        br.ReadUInt32();
+                        thingList.Add(ClientLoadoutOnlineThing.Deserialize(br));
                     }
-                    dump = true;
                 }
+
+                clo.ThingLists.Add(thingList);
             }
             
-#if DEBUG
-            if ( false && dump )
-            {
-                Console.WriteLine(br.GetBits(p, br.Position - p+50).ToBinaryString());
-            }
-#endif
             return clo;
+        }
+
+        public void Serialize(BitWriter bw)
+        {
+            bw.Write((byte)ThingLists.Count);
+            foreach (var thingList in ThingLists)
+            {
+                bw.Write((byte)thingList.Count);
+                foreach(var thing in thingList)
+                {
+                    thing.Serialize(bw);
+                    // "i >= 21" logic from Deserialize is handled automatically here. No special serialize logic needed.
+                }
+            }
         }
     }
 }
---
+++ RocketLeagueReplayParser/NetworkStream/ClientLoadoutOnlineThing.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketLeagueReplayParser.NetworkStream
+{
+    public class ClientLoadoutOnlineThing
+    {
+        const int MAX_UNKNOWN2 = 27; // This may need tweaking, but it works well enough for now
+
+        public UInt32 Unknown1 { get; private set; } // Wild ass guess - tied to material "expression" id?
+        public UInt32 Unknown2 { get; private set; }
+
+        public static ClientLoadoutOnlineThing Deserialize(BitReader br)
+        {
+            var clot = new ClientLoadoutOnlineThing(); // ha, "clot"
+            clot.Unknown1 = br.ReadUInt32();
+            clot.Unknown2 = br.ReadUInt32Max(MAX_UNKNOWN2);
+            return clot;
+        }
+
+        public void Serialize(BitWriter bw)
+        {
+            bw.Write(Unknown1);
+            bw.Write(Unknown2, MAX_UNKNOWN2);
+        }
+    }
+}
