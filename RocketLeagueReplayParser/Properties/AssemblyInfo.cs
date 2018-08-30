2339e2e0a32b6dc2be06d78d0acb2797a7260a44
Joe Bott <jjbott@gmail.com>
2018-08-29 20:01:43 -0500

    Updating version and readme

--- RocketLeagueReplayParser/Properties/AssemblyInfo.cs
+++ RocketLeagueReplayParser/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 
 // Incrementing minor for every major patch or major code change
 
-[assembly: AssemblyVersion("1.13.1.0")]
-[assembly: AssemblyFileVersion("1.13.1.0")]
-[assembly: AssemblyInformationalVersion("1.13.1")]
+[assembly: AssemblyVersion("1.14.0.0")]
+[assembly: AssemblyFileVersion("1.14.0.0")]
+[assembly: AssemblyInformationalVersion("1.14.0")]
